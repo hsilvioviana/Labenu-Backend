30a1970e8902644e8d7bc824ba6c3e2818ac72ae
hsilvioviana <hsilvioviana@hotmail.com>
2021-07-15 15:12:38 -0300

    Endpoint de Ver Musicas de uma Playlist Feito

---
+++ src/data/musics/getMusicsInPlaylist.ts
@@ -0,0 +1,24 @@
+import { post } from "../../model/musics"
+import { genresSplit } from "../../services/genresManager"
+import { connection } from "../connection"
+
+export const getMusicsInPlaylist = async (playlistId: string) : Promise<post[]> => {
+
+    const result = await connection.raw(`SELECT m.id, m.title, m.author,
+    DATE_FORMAT(m.releaseDate,'%d/%m/%Y') AS releaseDate , m.file, m.genres,
+    m.album, DATE_FORMAT(m.createdAt,'%d/%m/%Y') AS createdAt, 
+    DATE_FORMAT(m.updatedAt,'%d/%m/%Y') AS updatedAt
+    FROM LFS_MusicsPlaylistsRelations r LEFT JOIN LFS_Musics m 
+    ON r.musicId = m.id WHERE r.playlistId = "${playlistId}"`)
+
+    const posts: post[] = []
+
+    for (let post of result[0]) {
+
+        post = {...post, genres: genresSplit(post.genres) }
+
+        posts.push(post)
+    }
+
+    return posts
+}
--- src/routes/playlistsRouter.ts
+++ src/routes/playlistsRouter.ts
@@ -2,10 +2,12 @@ import { Router } from "express"
 import { getPlaylists } from "../controller/playlists/getPlaylists"
 import { playlistAddOrRemoveMusic } from "../controller/playlists/playlistAddOrRemoveMusic"
 import { playlistCreate } from "../controller/playlists/playlistCreate"
+import { playlistMusics } from "../controller/playlists/playlistMusics"
 
 
 export const playlistsRouter = Router()
 
 playlistsRouter.get("/", getPlaylists)
+playlistsRouter.get("/:id", playlistMusics)
 playlistsRouter.post("/create", playlistCreate)
 playlistsRouter.post("/change", playlistAddOrRemoveMusic)
